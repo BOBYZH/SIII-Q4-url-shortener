5e0912de3b3c7862a8ca61617e3aeb5e63b395cd
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-07-07 03:54:21 +0800

    Fix error of port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 // 資料庫套件設定
 const mongoose = require('mongoose')
